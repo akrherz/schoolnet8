10cca36a8a091443c75a56ea6d09ae5ad2885366
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-02-01 05:58:39 +0000

    Update

--- include/cameras.inc.php
+++ include/cameras.inc.php
@@ -7,7 +7,7 @@ $cameras = Array(
 		"name" => "Blank Park Zoo", "active" => true,
 		"ip" => "63.227.72.138", "county" => "Polk"),
   "SCAI4" => Array("sts" => mktime(0,0,0,10,25,2003), "ets" => mktime(4,30,0,5,11,2005),
-		"name" => "Carroll", "active" => false,
+		"name" => "Carroll", "active" => false, "removed" => true,
 		"iservice" => "Glidden REC", 
 		"iserviceurl" => "http://www.gliddenrec.com",
 		"ip" => "216.203.109.40", "county" => "Carroll"),
