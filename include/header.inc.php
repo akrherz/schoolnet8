d652c9f94398d19c06bdcc885b66673a6ea141d6
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-03-31 21:30:28 +0000

    Add a header include?

---
+++ include/header.inc.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>SchoolNet8 | <?php echo $TITLE; ?></title>
+ <style type="text/css" media="screen">@import "<?php echo $baseurl; ?>css/main.css";</style>
+ <style type="text/css" media="print">@import "<?php echo $baseurl; ?>css/print.css";</style>
+</head>
+<body>
