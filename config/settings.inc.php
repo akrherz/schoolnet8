10f800cb39d9c3bdbe5481d9ffe40ca64b164a23
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-03-31 19:23:36 +0000

    Now running Mapserver 4.8.2

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -9,17 +9,17 @@ $iemaccess = 'host=mesonet-db1.agron.iastate.edu dbname=iem user=nobody';
 //$server = 'http://www.schoolnet8.com';
 //$baseurl = 'http://www.schoolnet8.com/';
 //$basecgi = 'http://www.schoolnet8.com/cgi-bin/nwnwebsite/';
-$server = 'http://ics128-198.icsincorporated.com';
-$baseurl = 'http://ics128-198.icsincorporated.com/nwnwebsite/';
-$basecgi = 'http://ics128-198.icsincorporated.com/cgi-bin/nwnwebsite/';
-//$server = 'http://akrherz-laptop.agron.iastate.edu';
-//$baseurl = 'http://akrherz-laptop.agron.iastate.edu/nwnwebsite/';
-//$basecgi = 'http://akrherz-laptop.agron.iastate.edu/cgi-bin/nwnwebsite/';
+//$server = 'http://ics128-198.icsincorporated.com';
+//$baseurl = 'http://ics128-198.icsincorporated.com/nwnwebsite/';
+//$basecgi = 'http://ics128-198.icsincorporated.com/cgi-bin/nwnwebsite/';
+$server = 'http://akrherz-laptop.agron.iastate.edu';
+$baseurl = 'http://akrherz-laptop.agron.iastate.edu/nwnwebsite/';
+$basecgi = 'http://akrherz-laptop.agron.iastate.edu/cgi-bin/nwnwebsite/';
 $backupbaseurl = 'http://mesonet.agron.iastate.edu/nwnwebsite/';
 
 $nwnpath = '/home/akrherz/projects/nwnwebsite/';
 $mapfile = "$nwnpath/data/GIS/base.map";
 
 global $mapscript;
-$mapscript = 'php_mapscript_461.so';
+$mapscript = 'php_mapscript_482.so';
 ?>
