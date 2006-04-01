a874760c5dc9e7af07519b0b5488eb3c2018a6b8
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-04-01 02:41:58 +0000

    More small fixes.

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -10,15 +10,16 @@ $iemaccess = 'host=mesonet-db1.agron.iastate.edu dbname=iem user=nobody';
 //$baseurl = 'http://www.schoolnet8.com/';
 //$basecgi = 'http://www.schoolnet8.com/cgi-bin/nwnwebsite/';
 //$GMAPS_KEY = 'ABQIAAAArXt77YptylBNPFEy0AgJxBSWGjM97_n1DGuQJrTtOc2ryLPhTRT0ZEoBlAqspunGo75mvllNuO-ZNg';
-//$server = 'http://ics128-198.icsincorporated.com';
-//$baseurl = 'http://ics128-198.icsincorporated.com/nwnwebsite/';
-//$basecgi = 'http://ics128-198.icsincorporated.com/cgi-bin/nwnwebsite/';
-//$GMAPS_KEY = 'ABQIAAAArXt77YptylBNPFEy0AgJxBTvX-LZuEomi989jNN1ZEOlwjWzvxQ7MGfNs8Y2x60Nz7OWub34lSLt2Q';
+$server = 'http://ics128-198.icsincorporated.com';
+$baseurl = 'http://ics128-198.icsincorporated.com/nwnwebsite/';
+$basecgi = 'http://ics128-198.icsincorporated.com/cgi-bin/nwnwebsite/';
+$GMAPS_KEY = 'ABQIAAAArXt77YptylBNPFEy0AgJxBSzbbY03gQRbQMgQLHHOrDAm4WHnhQ998medELI5ixmUfXp1bAy708-Fw';
+/*
 $server = 'http://akrherz-laptop.agron.iastate.edu';
 $baseurl = 'http://akrherz-laptop.agron.iastate.edu/nwnwebsite/';
 $basecgi = 'http://akrherz-laptop.agron.iastate.edu/cgi-bin/nwnwebsite/';
 $GMAPS_KEY = 'ABQIAAAArXt77YptylBNPFEy0AgJxBQc0z_IWnX6xkK5sZtx96r-WEKufhSob0tQSwJqHMrQWKgTHS1brlX0Eg';
-
+*/
 $backupbaseurl = 'http://mesonet.agron.iastate.edu/nwnwebsite/';
 
 $nwnpath = '/home/akrherz/projects/nwnwebsite/';
