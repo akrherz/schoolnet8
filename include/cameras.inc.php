8d8f74ece98bc251ea932b7887808b7ce220acb3
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-06-27 19:05:55 +0000

    Bug!

--- include/cameras.inc.php
+++ include/cameras.inc.php
@@ -15,7 +15,7 @@ $cameras = Array(
         "name" => "Chariton", "active" => true, 
         "ip" => "69.66.60.199", "county" => "Lucas"),
   "SCGI4" => Array("sts" => mktime(13,0,0,6,27,2006), "ets" => time(),
-        "name" => "Clarion", "active" => true, "removed" => false,
+        "name" => "Clarion", "active" => true,
 		"iservice" => "Paradigm Benefits", 
 		"iserviceurl" => "http://www.paradigmbenefit.com",
         "ip" => "12.207.138.18", "county" => "Wright"),
