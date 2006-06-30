1654ffebeaeff14ee8aa35c2fbcea113bdb784b4
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-06-30 16:42:14 +0000

    Fix bad functions

--- include/imagemap.inc.php
+++ include/imagemap.inc.php
@@ -3,20 +3,21 @@
 function kcciSelectAuto($selected, $pre, $post, $target=""){
 
 include_once("locs.inc.php");
+global $Scities;
 if (strlen($target) == 0) { 
   $s = "<form name=\"autoselect\" action=\"#\"><select name=\"station\" onchange=\"location=this.form.station.options[this.form.station.selectedIndex].value\">\n";
 } else {
   $s = "<form name=\"autoselect\" action=\"#\"><select name=\"station\" onchange=\"window.open(this.form.station.options[this.form.station.selectedIndex].value, 'basewindow');\">\n";
 }
 
-for ($i = 0; $i < count($Scities); $i++) {
-  $city = current($Scities);
-  $s .= "<option value=\"". $pre . $city["id"] . $post ."\"";
-  if (strcmp($selected, $city["id"]) == 0){
+reset($Scities);
+while( list($id,$d) = each($Scities) )
+{
+  $s .= "<option value=\"". $pre . $id . $post ."\"";
+  if (strcmp($selected, $id) == 0){
         $s .= " selected=\"selected\" ";
   }
-  $s .= " >". $city["city"] ."</option>\n";
-  next($Scities);
+  $s .= " >". $d["city"] ."</option>\n";
 }
 
 $s .= "</select></form>\n";
@@ -27,16 +28,17 @@ return $s;
 function kcciSelect($selected){
 
 include_once("locs.inc.php");
+global $Scities;
 echo "<select name=\"station\">\n";
 
-for ($i = 0; $i < count($Scities); $i++) {
-  $city = current($Scities);
-  echo "<option value=\"". $city["id"] ."\"";
-  if ($selected == $city["id"]){
+reset($Scities);
+while( list($id,$d) = each($Scities) )
+{
+  echo "<option value=\"". $id ."\"";
+  if ($selected == $id){
         echo " selected=\"selected\" ";
   }
-  echo " >". $city["city"] ."</option>\n";
-  next($Scities);
+  echo " >". $d["city"] ."</option>\n";
 }
 
 echo "</select>\n";
