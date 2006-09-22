f1aee001a66e311023606d066d9f1e149476bf40
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-09-22 03:39:44 +0000

    Add sponsor

--- include/cameras.inc.php
+++ include/cameras.inc.php
@@ -67,6 +67,8 @@ $cameras = Array(
   "SKNI4" => Array("sts" => mktime(17,0,0,9, 26,2005), 
         "ets" => time(),
 		"name" => "Knoxville", "active" => true,
+		"hosted" => "Sideways Karting Center",
+		"hostedurl" => "http://www.slidewayskartingcenter.com",
 		"ip" => "71.7.58.111", "county" => "Marion"),
   "SNEI4" => Array("sts" => mktime(19,0,0, 4,16,2004), "ets" => time(),
 		"name" => "Newton", "active" => true,
