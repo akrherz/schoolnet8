9052e8281e02087a62753401c4ceb68b1b8eb0f7
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2006-10-09 18:41:27 +0000

    Google Analytics

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -3,6 +3,11 @@ SchoolNet8.com sponsored by: <a href="<?php echo $baseurl; ?>tool/clicktru.php?s
 &nbsp; &nbsp; Page Loaded: <?php echo date("d F Y  h:i A"); ?> [<a href="<?php echo $baseurl; ?>contact.phtml">Contact Info</a>]
 </div>
 </div>
-
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-784549-1";
+urchinTracker();
+</script>
 </body>
 </html>
