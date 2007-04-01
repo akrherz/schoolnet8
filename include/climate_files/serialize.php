bdf31a28ca4217d3cfa40a6a0524387e5735ec10
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2007-04-01 00:32:46 +0000

    Error with climate database :(

--- include/climate_files/serialize.php
+++ include/climate_files/serialize.php
@@ -1,5 +1,6 @@
 <?php
 /* Serialize the climate data to files we can update! */
+set_time_limit(580);
 $pg = pg_connect("user=nobody dbname=coop host=mesonet-db1.agron.iastate.edu");
 
 $rs = pg_query($pg, "SELECT * from climate51 ORDER by station, valid ASC");
@@ -13,7 +14,7 @@ for( $i=0; $row = @pg_fetch_array($rs,$i); $i++)
   $v = $row["valid"];
   $p = floatval($row["precip"]);
   $k = sprintf("%s-%s", substr($v,0,7), $id);
-  echo "$k - $cur - $v - $q \n";
+  //echo "$k - $cur - $v - $q \n";
   if ($k == $cur){ $q += $p; }
   else {$q = $p;}
   $cur = $k;
@@ -21,10 +22,10 @@ for( $i=0; $row = @pg_fetch_array($rs,$i); $i++)
   $row["mtd"] = $q;
   $db[ $row["valid"] ][$id] = $row;
 }
-
 reset($db);
 while( list($id,$d) = each($db))
 {
+ echo $id;
   $s = serialize($d);
   $fp =fopen($id.".txt", 'w');
   fwrite($fp,$s);
