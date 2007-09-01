a37e26a328e7dace2a5ba92eb59bf10c6c6d8c97
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2007-09-01 04:24:19 +0000

    September updates, 3 training videos!

---
+++ html/training/menu.php
@@ -0,0 +1,26 @@
+<?php
+
+$pages = Array(
+ "website" => "Website Tour [14 MB] (9 minutes)",
+ "1minutetrace" => "1 Minute Time Series Graphs [2 MB] (6 minutes)",
+ "snet8viewer" => "SchoolNet8 Desktop Viewer Install/Use [3 MB] (4 minutes)",
+);
+
+
+function lessonSelect($selected)
+{
+ global $pages, $baseurl;
+ $s = "<form method=\"GET\" action=\"index.phtml\" name=\"sel\">\n";
+ $s .= "<strong>Select Video: </strong><select name=\"module\" onChange=\"location=this.form.module.options[this.form.module.selectedIndex].value;\">\n";
+ reset($pages);
+ while (list($key,$val) = each($pages))
+ {
+   $s .= "<option value=\"$baseurl/training/$key/index.phtml\" ";
+   if ($selected == $key) $s .= "SELECTED";
+   $s .= ">$val</option>\n";
+ }
+ $s .= "</select>\n";
+ return $s;
+}
+
+?>
