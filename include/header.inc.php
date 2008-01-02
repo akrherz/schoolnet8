b641b81b04f8a95b203cd666cbf11104e4a70ef4
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-01-02 19:27:30 +0000

    bugs

--- include/header.inc.php
+++ include/header.inc.php
@@ -5,6 +5,6 @@
   <title>SchoolNet8 | <?php echo $TITLE; ?></title>
  <style type="text/css" media="screen">@import "<?php echo $baseurl; ?>css/main.css";</style>
  <style type="text/css" media="print">@import "<?php echo $baseurl; ?>css/print.css";</style>
-  <script type="text/javascript" src="<?php echo $baseurl; ?>/js/common.js?v=101"></script>
+  <script type="text/javascript" src="<?php echo $baseurl; ?>/js/common.js?v=102"></script>
 </head>
 <body>
