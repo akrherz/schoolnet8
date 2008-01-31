bf099f7a5eedad0f83f6ae0d0a0b779a4e03605b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-01-31 19:23:25 +0000

    more xhtml

--- scripts/genFX.py
+++ scripts/genFX.py
@@ -84,7 +84,7 @@ def generator(sid, lat, lon):
 
   #for i in range(len(ta['k-p12h-n14-3'])):
   o = open('../data/fx/%s.html' % (sid,) , 'w')
-  o.write("<table cellspacing=0 cellpadding=1 width=\"670\">")
+  o.write("<table cellspacing=\"0\" cellpadding=\"1\" width=\"670\">")
   o.write("<tr>")
   for i in range(9):
     ts = ta['k-p12h-n14-3'][i]
