1aee9d67e5ee401a38c62748ce6c362eff9f852d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-07-21 18:06:45 +0000

    FIXME

--- html/GIS/apps/plotter/dl.php
+++ html/GIS/apps/plotter/dl.php
@@ -5,5 +5,5 @@ $img = isset($_GET["img"]) ? $_GET["img"] : die("No Image specified");
  header("Content-type: application/octet-stream");
  header("Content-Disposition: attachment; filename=image.png");
 if ( strlen($img) > 30 ) die();
-readfile("/home/httpd/html/$img");
+//readfile("/home/httpd/html/$img");
 ?>
