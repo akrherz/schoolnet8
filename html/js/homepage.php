316f8f197a49083c168ad9658d0f7c0a2901d107
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-01-01 04:51:31 +0000

    again, stupid IE

--- html/js/homepage.php
+++ html/js/homepage.php
@@ -8,7 +8,7 @@ var map;
 function updater(){
   var d = new Date();
   document.getElementById("lsdimage").innerHTML = '<a href="<?php echo $baseurl; ?>/site.phtml?station='+ selectedSite +'"><img src="<?php echo $baseurl; ?>static/radar/'+ selectedSite +'/'+ selectedSite +'_0.png?'+ d.getTime() +'" border="1" width="320" height="240" alt="Live Super Doppler" style=\"float: right;\"/></a><br />Click on map for more details.';
-  document.getElementById("webcamimage").innerHTML = '<a href="<?php echo $baseurl; ?>/camera/"><img src="<?php echo $baseurl; ?>camera/stills/'+ eval("webcamlookup."+ selectedSite ) +'.jpg?'+ d.getTime() +'" border="1" width="320" height="240" alt="Webcam" /></a><br />Click on image for other cameras.';
+  document.getElementById("webcamimage").innerHTML = '<a href="<?php echo $baseurl; ?>/camera/"><img src="<?php echo $baseurl; ?>camera/stills/'+ eval("webcamlookup."+ selectedSite ) +'.jpg?'+ d.getTime() +'" border="1" width="320" height="240" alt="Webcam" style=\"float: right;\"/></a><br />Click on image for other cameras.';
 
 };
 
