6f7e9972c9900308187c47b626cb4fc1bd703b8d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-03-31 19:18:19 +0000

    I have something that works, yippeee, I think

---
+++ html/js/site.js
@@ -0,0 +1,66 @@
+Ext.onReady(function(){
+
+var tempRenderer = new function(v){ return v +" F"; }
+
+var form = new Ext.FormPanel({
+    title     : 'Live Observation',
+    renderTo  : 'datagrid',
+    id        : 'propGrid',
+    url       : '/json/get-site?site='+ nwsli,
+    reader    : new Ext.data.JsonReader({
+	    root : 'data'
+	    },
+	    Ext.data.Record.create([
+		  {name: 'ts'},
+		  {name: 'tmpf', renderer: tempRenderer},
+		  {name: 'dwpf'},
+		  {name: 'relh'},
+		  {name: 'xtmpf'},
+		  {name: 'feel'},
+		  {name: 'xsrad'},
+		  {name: 'srad'},
+		  {name: 'xdrct'},
+		  {name: 'xsped'},
+		  {name: 'sped'},
+		  {name: 'drct'},
+		  {name: 'pday'},
+		  {name: 'pmonth'},
+		  {name: 'ntmpf'}
+	    ])
+	),
+    labelWidth: 175,
+    defaults  : {
+        readOnly   : true,
+        xtype      : 'textfield',
+        style      : 'border:0px; background:#fff;'
+    },
+    items     : [
+      {fieldLabel : 'Timestamp', id : 'ts'},
+      {fieldLabel : 'Air Temperature [F]', id : 'tmpf'},
+      {fieldLabel : 'Dew Point Temp [F]', id : 'dwpf'},
+      {fieldLabel : 'Relative Humidity [%]', id : 'relh'},
+      {fieldLabel : "Feel's Like [F]", id : 'feel'},
+      {fieldLabel : "Wind Speed [mph]", id : 'sped'},
+      {fieldLabel : "Wind Direction", id : 'drct'},
+      {fieldLabel : "High Wind Speed [mph]", id : 'xsped'},
+      {fieldLabel : "High Wind Speed Dir", id : 'xdrct'},
+      {fieldLabel : 'High Temperature [F]', id : 'xtmpf', 
+        style: 'color: #f00; border:0px; background:#fff;'},
+      {fieldLabel : 'Low Temperature [F]', id : 'ntmpf',
+        style: 'color: #00f; border:0px; background:#fff;'},
+      {fieldLabel : 'Solar Radiation [W/m**2]', id : 'srad'},
+      {fieldLabel : 'High Solar Radiation [W/m**2]', id : 'xsrad'},
+      {fieldLabel : 'Today Rainfall [inch]', id : 'pday'},
+      {fieldLabel : 'Monthly Rainfall [inch]', id : 'pmonth'}
+    ]
+});
+
+var task = {
+  run: function(){
+       form.load();
+  },
+  interval: 7000
+}
+Ext.TaskMgr.start(task);
+
+});
