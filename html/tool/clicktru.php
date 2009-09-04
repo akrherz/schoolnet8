09ac18f44771b1784ae7a506e577b4526e1d4232
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-09-04 19:17:28 +0000

    support webcam clicks

--- html/tool/clicktru.php
+++ html/tool/clicktru.php
@@ -1,16 +1,19 @@
 <?php
+include("../../config/settings.inc.php");
+include("$nwnpath/include/sponsors.inc.php");
+include("$nwnpath/include/cameras.inc.php");
+$REMOTE_ADDR = getenv('REMOTE_ADDR');
+$station = isset($_GET["station"]) ? substr($_GET['station'],0,11): die();
 
- include("../../config/settings.inc.php");
- include("../../include/sponsors.inc.php");
-  $REMOTE_ADDR = getenv('REMOTE_ADDR');
-  $station = $_GET['station'];
+$c = pg_connect($dbhost);
+pg_exec($c, "INSERT into clicktru(station, ip) VALUES
+             ('". $station ."', '". $REMOTE_ADDR ."')");
 
-  $c = pg_connect($dbhost);
+if (! array_key_exists($station, $sponsors)){
+ $sponsorurl = $cameras[$station]["sponsorurl"];
+} else {
+ $sponsorurl = $sponsors[$station]["url"];
+}
 
-  pg_exec($c, "INSERT into clicktru(station, ip) VALUES
-    ('". $station ."', '". $REMOTE_ADDR ."')");
-
-  pg_close($c);
-
-Header("Location: ".$sponsors[$station]["url"] )
+Header("Location: $sponsorurl")
 ?>
