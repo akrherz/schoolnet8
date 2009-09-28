7e9fd4c2ed4239db5bb56ad35a73e463c7dcda0b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-09-28 13:55:58 +0000

    Include sponsor info on these pages!

--- html/js/webcams.php
+++ html/js/webcams.php
@@ -10,6 +10,34 @@ while(list($sid, $ar) = each($cxref)){
   $data .= sprintf("%s: '%s',", $sid, $ar[0]);
 }
 echo substr($data,0,-1);
+echo "};\n";
+
+/* Now we output a dictionary of sponsor stuff! */
+echo "webcamsponsor = {";
+$data = "";
+while(list($sid, $ar) = each($cameras)){
+  $d = "";
+  if ($ar["sponsor"] != ""){
+     $d .= "Sponsored by: <a href=\"". $ar["sponsorurl"] ."\">". $ar["sponsor"] ."</a><br />";
+  }
+
+  if ($ar["iservice"] != "" ){
+     $d .= "Internet service by: <a href=\"". $ar["iserviceurl"] ."\">". $ar["iservice"] ."</a><br />";
+  }
+
+  if ($ar["hosted"] != "")
+  {
+     if ($ar["hostedurl"] != ""){
+       $d .= "Hosted by: <a href=\"". $ar["hostedurl"] ."\">". $ar["hosted"] ."</a><br />";
+      }
+      else {
+       $d .= "Hosted by: <b>". $ar["hosted"] ."</b><br />";
+      }
+  }
+  $data .= sprintf("%s: '%s',\n", str_replace("-", "_", $sid), $d);
+
+}
+echo substr($data,0,-2);
 echo "};";
 
 ?>
