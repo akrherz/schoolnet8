b44a5937df613495f8d8de59ffcfc33288af8a32
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-12-31 19:49:20 +0000

    add another module, enough for now to get people's appetites wet

---
+++ html/camera/movies/ltemplates/2.php
@@ -0,0 +1,39 @@
+<h3><?php echo $appdict[$v]["title"]; ?></h3>
+
+<h4>Description:</h4>
+<p>While somewhat hard to see, the top of rainbow is shown slowly rising
+above the horizon. This timelapse is from the Clarion, Iowa webcamera.</p>
+
+<h4>Video:</h4>
+<?php echo $player; ?>
+
+<h4>Questions:</h4>
+<ol>
+ <li>Why does the rainbow appear to rise?</li>
+ <li>Why don't we see the entire rainbow (full circle)?  Why is only the 
+     only part of the rainbow we see in the sky?</li>
+</ol>
+
+<h4>Discussion:</h4>
+
+<p>The video shows a rainbow that appears to rise above the horizon.  The
+motion is hard to notice when watching a rainbow without the help of speeding
+up time as in this movie.  A previous module, discussed the 
+<a href="learn.phtml?v=1">left to right</a> movement of a rainbow and the
+same material applies in this case.  The vertical movement is a result of
+a change in solar 
+<a href="http://en.wikipedia.org/wiki/Zenith">zenith angle</a>, 
+which is just a fancy term for how high in the sky off the horizon 
+the sun appears.  So as the sun sets in the west, the angle changes and the
+rainbow will appear higher in the sky.
+
+<h4>Links to Internet Materials:</h4>
+<ul>
+ <li><a href="http://en.wikipedia.org/wiki/Rainbow">Wikipedia Rainbow Article</a></li>
+ <li><a href="http://en.wikipedia.org/wiki/Sun">Wikipedia Sun Article</a></li>
+ <li><a href="http://en.wikipedia.org/wiki/Sundial">Wikipedia Sundial Article</a></li>
+ <li><a href="http://eo.ucar.edu/rainbows/">NCAR's About Rainbows</a></li>
+ <li><a href="http://www.csiro.au/resources/ps2ab.html">Tracing the path of the Sun</a></li>
+</ul>
+
+<p>Do you have corrections or additions to this material?  Please <a href="../../contact.phtml">contact us</a>, thanks!
