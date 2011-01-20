a764328d30e43c38967228d86759ae8793f5b5e5
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-01-20 02:06:59 +0000

    completely rewrite, no more bugs, I hope

--- scripts/genFX.py
+++ scripts/genFX.py
@@ -1,129 +1,107 @@
 #!/mesonet/python/bin/python
 
-from xml.dom.ext.reader import Sax2
-from xml.dom.ext import StripXml
-from xml import xpath
 import re, pdb, datetime, urllib, time, sys, shutil, traceback
 import mx.DateTime
+from xml.etree import ElementTree
+
+ENDPOINT = "http://www.weather.gov/forecasts/xml/SOAP_server/ndfdSOAPclientByDay.php?"
 
 def generator(sid, lat, lon):
 
-  now = datetime.datetime.now()
-  rest_uri = "http://www.weather.gov/forecasts/xml/SOAP_server/ndfdSOAPclientByDay.php?lat=%s&lon=%s&format=12+hourly&startDate=%s&numDays=7&Submit=Submit" % (lat, lon, now.strftime("%Y-%m-%d") )
-  urllib.urlretrieve(rest_uri, 'tmp.xml')
-  x = open('tmp.xml').read()
-  x = re.sub(">(\W){2,}<", "><", x.replace("\n", "") )
+    now = datetime.datetime.now()
+    rest_uri = "%slat=%s&lon=%s&format=12+hourly&startDate=%s&numDays=7&Submit=Submit" % (
+                                                 ENDPOINT, lat, lon, now.strftime("%Y-%m-%d") )
+    doc = ElementTree.XML( urllib.urlopen(rest_uri).read() )
 
-  try:
-    reader = Sax2.Reader()
-    doc = StripXml(reader.fromString(x))
-  except:
-    traceback.print_exc(file=sys.stdout)
-    return
-
-  # Load up timearrays
-  ta = {}
-  tnames = {}
-  a = xpath.Evaluate('dwml/data/time-layout', doc)
-  for elem in a:
-    q = xpath.Evaluate('layout-key', elem)
-    lk = q[0]._get_childNodes()[0].data
-
-    sts = xpath.Evaluate('start-valid-time', elem)
-    ta[lk] = [0] * len(sts)
-    for i in range(len(sts)):
-      dt = datetime.datetime.fromtimestamp(time.mktime(time.strptime(sts[i]._get_childNodes()[0].data[:16], '%Y-%m-%dT%H:%M'))) 
-      ta[lk][i] = dt
-      tnames[dt] = sts[i].getAttribute("period-name")
-
-  # Max Temps
-  #  maximum, minimum
-  temps = {}
-  a = xpath.Evaluate('dwml/data/parameters/temperature', doc)
-  for elem in a:
-    thisTemp = elem.getAttribute("type")
-    temps[ thisTemp ] = {}
-    thisTimeAxis = elem.getAttribute("time-layout")
-    #print thisTemp
-    nodelist = elem._get_childNodes()
-    for i in range(1, len(nodelist)):
-      if (len(nodelist[i]._get_childNodes()) > 0):
-        temps[thisTemp][ ta[thisTimeAxis][i-1] ] = nodelist[i]._get_childNodes()[0].data 
-      else:
-        temps[thisTemp][ ta[thisTimeAxis][i-1] ] = -99
-
-
-  # weather
-  #  wind
-  weather = {}
-  a = xpath.Evaluate('dwml/data/parameters/weather', doc)
-  for elem in a:
-    nodelist = elem._get_childNodes()
-    thisTimeAxis = elem.getAttribute("time-layout")
-    for i in range(1, len(nodelist)):
-      weather[ ta[thisTimeAxis][i-1] ] = (nodelist[i]).getAttribute("weather-summary")
-
-  # Conditions
-  #  forecast-NWS
-  icons = {}
-  a = xpath.Evaluate('dwml/data/parameters/conditions-icon', doc)
-  for elem in a:
-    thisTemp = elem.getAttribute("type")
-    thisTimeAxis = elem.getAttribute("time-layout")
-    #print thisTemp
-    nodelist = elem._get_childNodes()
-    for i in range(1, len(nodelist)):
-      l = nodelist[i]._get_childNodes()
-      if (len(l) == 0):
-        #print 'nill', ta[thisTimeAxis][i-1]
-        continue
-      else:
-        icons[ ta[thisTimeAxis][i-1] ] = l[0].data
-
-  #print temps
-  #print weather
-  #print icons
-  if not ta.has_key('k-p12h-n14-3'):
-    #print ta.keys()
-    return
-
-  #for i in range(len(ta['k-p12h-n14-3'])):
-  o = open('../data/fx/%s.html' % (sid,) , 'w')
-  o.write("%s" % (" "*int( mx.DateTime.now().second),))
-  o.write("<table cellspacing=\"0\" cellpadding=\"1\" width=\"640\">")
-  o.write("<tr>")
-  for i in range(9):
-    ts = ta['k-p12h-n14-3'][i]
-    o.write("<th width=\"11%%\">%s</th>" % (tnames[ts],))
-  o.write("</tr>\n")
-
-  o.write("<tr>")
-  for i in range(9):
-    ts = ta['k-p12h-n14-3'][i]
-    if not icons.has_key(ts):
-        print 'Missing ICONS TS %s\n Keys: %s' % (ts, icons.keys() )
-        o.write("<td></td>")
-    else:
-        o.write("<td><img src=\"%s\" alt=\"fx\"/></td>" % (icons[ts],))
-  o.write("</tr>\n")
-
-  o.write("<tr>")
-  for i in range(9):
-    ts = ta['k-p12h-n14-3'][i]
-    o.write("<td>%s</td>" % (weather[ts],))
-  o.write("</tr>\n")
-
-  o.write("<tr>")
-  for i in range(9):
-    ts = ta['k-p12h-n14-3'][i]
-    if (temps['maximum'].has_key(ts)):
-      o.write("<td>Hi %s&deg;F</td>" % (temps['maximum'][ts],) )
-    else:
-      o.write("<td>Lo %s&deg;F</td>" % (temps['minimum'][ts],) )
-  o.write("</tr>\n")
-
-  o.write("</table>")
-  o.close()
+    taxis = {}
+    tnames = {}
+    for elem in doc.findall("./data/time-layout"):
+        key = elem.find("layout-key").text
+        taxis[key] = []
+        tnames[key] = []
+        for elem2 in elem.findall("./start-valid-time"):
+            ts = mx.DateTime.strptime( elem2.text[:16], '%Y-%m-%dT%H:%M') 
+            taxis[key].append( ts )
+            tnames[key].append( elem2.attrib.get("period-name",None) )
+    
+    temps = {}
+    for elem in doc.findall("./data/parameters/temperature"):
+        name = elem.find("./name")
+        temps[ name.text ] = {'taxis': elem.attrib['time-layout'], 
+                              'vals': []}
+        for v in elem.findall("./value"):
+            temps[ name.text ]['vals'].append( v.text )
+    
+    
+    for elem in doc.findall("./data/parameters/weather"):
+        weather = {'taxis': elem.attrib['time-layout'], 
+                              'vals': []}
+        for v in elem.findall("./weather-conditions"):
+            weather['vals'].append( v.attrib.get('weather-summary',None) )
+    
+    for elem in doc.findall("./data/parameters/conditions-icon"):
+        icons = {'taxis': elem.attrib['time-layout'], 
+                              'vals': []}
+        for v in elem.findall("./icon-link"):
+            icons['vals'].append( v.text )
+    
+    data = {}
+    for val, tm in zip( temps['Daily Maximum Temperature']['vals'],
+                tnames[ temps['Daily Maximum Temperature']['taxis'] ] ):
+        if not data.has_key(tm):
+            data[tm] = {'high': None, 'low': None, 'weather': None, 'icon': None}
+        data[tm]['high'] = val
+    
+    for val, tm in zip( temps['Daily Minimum Temperature']['vals'],
+                tnames[ temps['Daily Minimum Temperature']['taxis'] ] ):
+        if not data.has_key(tm):
+            data[tm] = {'high': None, 'low': None, 'weather': None, 'icon': None}
+        data[tm]['low'] = val
+    
+    for val, tm in zip( icons['vals'],
+                tnames[ icons['taxis'] ] ):
+        if not data.has_key(tm):
+            data[tm] = {'high': None, 'low': None, 'weather': None, 'icon': None}
+        data[tm]['icon'] = val
+    
+    for val, tm in zip( weather['vals'],
+                tnames[ weather['taxis'] ] ):
+        if not data.has_key(tm):
+            data[tm] = {'high': None, 'low': None, 'weather': None, 'icon': None}
+        data[tm]['wather'] = val
+    
+    
+    #print "MAX TEMPS", tnames[ temps['Daily Maximum Temperature']['taxis'] ]
+    #print "MIN TEMPS", tnames[ temps['Daily Minimum Temperature']['taxis'] ]
+    #print "WEATHER", tnames[ weather['taxis'] ]
+    #print "ICONS", tnames[ icons['taxis'] ]
+    
+    o = open('%s.html' % (sid,), 'w')
+    o.write("<table cellspacing=\"0\" cellpadding=\"1\" width=\"640\">")
+    
+    o.write("<tr>")
+    for tm in tnames[ icons['taxis'] ][:9]:
+        o.write("<th width=\"11%%\">%s</th>" % (tm, ) )
+    o.write("</tr>")
+    
+    o.write("<tr>")
+    for tm in tnames[ icons['taxis'] ][:9]:
+        o.write("<td><img src=\"%s\" alt=\"fx\"/></td>" % (data[tm]['icon'], ) )
+    o.write("</tr>")
+    
+    o.write("<tr>")
+    for tm in tnames[ icons['taxis'] ][:9]:
+        if data[tm]['high'] is not None:
+            d = data[tm]['high']
+            l = 'Hi'
+        else:
+            d = data[tm]['low']
+            l = 'Lo'
+        o.write("<td>%s %s&deg;F</td>" % (l, d) )
+    o.write("</tr>")
+    
+    o.write("</table>")
+    o.close()
 
 if (__name__ == "__main__"):
   generator(sys.argv[1], sys.argv[2], sys.argv[3])
