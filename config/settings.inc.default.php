1e10c31947fdb2acd0f947d279823aef4ef635de
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-12-29 16:07:42 +0000

    remove settings.inc.php from svn

---
+++ config/settings.inc.default.php
@@ -0,0 +1,21 @@
+<?php
+date_default_timezone_set('America/Chicago');
+
+$dbhost = 'host=127.0.0.1 dbname=kcci user=akrherz';
+$summarydb = 'host=mesonet.agron.iastate.edu dbname=summary user=nobody';
+$iemkccidb = 'host=mesonet.agron.iastate.edu dbname=kcci user=nobody';
+$iemdbhost = 'host=mesonet.agron.iastate.edu user=nobody dbname=snet';
+$iemaccess = 'host=mesonet.agron.iastate.edu dbname=iem user=nobody';
+$mesosite = 'host=mesonet.agron.iastate.edu dbname=mesosite user=nobody';
+
+$server = 'http://schoolnet8.local';
+$baseurl = 'http://schoolnet8.local/';
+$basecgi = 'http://schoolnet8.local/cgi-bin/';
+$GMAPS_KEY = 'ABQIAAAArXt77YptylBNPFEy0AgJxBTKM1pydaAG3ALFS_17Yg_JzDhv_hSQNTnSOgubglUfDHP1Nnwy-r-EsQ';
+
+$backupbaseurl = 'http://iem.schoolnet8.com/';
+
+$nwnpath = '/home/akrherz/projects/nwnwebsite/';
+$mapfile = "$nwnpath/data/GIS/base.map";
+
+?>
