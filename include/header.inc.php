14f15a476dac5aade169119f1d5aa63feec7dcf0
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-12-30 18:07:55 +0000

    add apple touch icon

--- include/header.inc.php
+++ include/header.inc.php
@@ -2,10 +2,11 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>SchoolNet8 | <?php echo $TITLE; ?></title>
+ <title>SchoolNet8 | <?php echo $TITLE; ?></title>
  <style type="text/css" media="screen">@import "<?php echo $baseurl; ?>css/main.css?v=0.0.2";</style>
  <style type="text/css" media="print">@import "<?php echo $baseurl; ?>css/print.css";</style>
-  <script type="text/javascript" src="<?php echo $baseurl; ?>/js/common.js?v=102"></script>
-  <?php if (isset($HEADEXTRA)) echo $HEADEXTRA; ?>
+ <script type="text/javascript" src="<?php echo $baseurl; ?>/js/common.js?v=102"></script>
+ <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+ <?php if (isset($HEADEXTRA)) echo $HEADEXTRA; ?>
 </head>
 <body <?php if (isset($BODYEXTRA)) echo $BODYEXTRA; ?>>
