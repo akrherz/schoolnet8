e6aab029ce2f38531e41e3402c5927004fb9f0be
akrherz <akrherz@iastate.edu>
2014-03-11 08:40:48 -0500

    set temperature data missing when it is out of bounds

--- html/js/site.v2.js
+++ html/js/site.v2.js
@@ -8,15 +8,24 @@ function fetcher(){
 		success: function(data){
 			//console.log(data);
 			$('#ds_ts').text( data.data[0].ts );
-			$('#ds_tmpf').text( data.data[0].tmpf );
-			$('#ds_ntmpf').text( data.data[0].ntmpf );
-			$('#ds_xtmpf').text( data.data[0].xtmpf );
-			$('#ds_dwpf').text( data.data[0].dwpf );
+			if (data.data[0].tmpf > -50 && data.data[0].tmpf < 120){
+				$('#ds_tmpf').text( data.data[0].tmpf );
+				$('#ds_ntmpf').text( data.data[0].ntmpf );
+				$('#ds_xtmpf').text( data.data[0].xtmpf );
+				$('#ds_dwpf').text( data.data[0].dwpf );
+				$('#ds_relh').text( data.data[0].relh );
+				$('#ds_feel').text( data.data[0].feel );
+			} else {
+				$('#ds_tmpf').text('--');
+				$('#ds_ntmpf').text('--');
+				$('#ds_xtmpf').text('--');
+				$('#ds_dwpf').text('--');
+				$('#ds_relh').text('--');
+				$('#ds_feel').text('--');				
+			}
 			$('#ds_srad').text( data.data[0].srad );
 			$('#ds_drct').text( data.data[0].drct );
 			$('#ds_sped').text( data.data[0].sped );
-			$('#ds_relh').text( data.data[0].relh );
-			$('#ds_feel').text( data.data[0].feel );
 			$('#ds_pday').text( data.data[0].pday );
 			$('#ds_pmonth').text( data.data[0].pmonth );
 			$('#ds_pres').text( data.data[0].pres );
