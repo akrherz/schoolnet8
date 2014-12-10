81ab59d994183d64cd8d668ec5bd3718ace10e7d
akrherz <akrherz@iastate.edu>
2014-12-10 15:33:49 -0600

    fix warnings call

--- scripts/genstills.php
+++ scripts/genstills.php
@@ -35,8 +35,8 @@ foreach($locs->table as $key => $value)
   $counties->set("status", MS_ON);
 
   $warnings_c = $map->getlayerbyname("postgis_warnings_c");
-  $warnings_c->set('data', sprintf("geom from (select phenomena, geom, oid 
-  	from warnings_%s WHERE expire > now() 
+  $warnings_c->set('data', sprintf("geom from (select phenomena, geom, w.oid
+  	from warnings_%s w JOIN ugcs u on (u.gid = w.gid) WHERE expire > now() 
   	and phenomena in ('SV','TO','FF') ORDER by expire, phenomena ASC) as foo 
   	 using unique oid using SRID=4326", date("Y")));
   $warnings_c->set("status", MS_ON);
